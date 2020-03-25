e6570fd1d43dc96c457072628812ba8d03843496
therealmaxkim <gk1473@nyu.edu>
2020-03-25 15:38:51 -0400

    finaal frontend merge

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import App from "./js/components/App";
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   mode: 'production',
   context: path.join(__dirname, './'),
-  entry: './app/app.jsx',
+  entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
@@ -14,11 +14,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        loader: 'jsx-loader',
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        include: path.join(__dirname, 'app'),
-      },
-    ],
+        use: {
+          loader: "babel-loader"
+        },
+        include: path.join(__dirname, 'src'),
+      }
+    ]
   },
 };
